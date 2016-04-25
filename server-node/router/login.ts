05d5f9668be0932592400fd92e98908cb52af712
kwon dong jun <mayajuni@naver.com>
2016-04-25 17:02:21 +0900

    로그인 작업 진행(테스트 전) 로큰 로그인 및 아이디 패스워드 로그인 추가

--- server-node/router/login.ts
+++ server-node/router/login.ts
@@ -2,9 +2,44 @@
  * Created by mayaj on 2016-04-24.
  */
 import {Router} from "express";
+import {LoginService} from "../service/loginService";
+import johayoPvs = require("johayo-pvs");
 
+let wrap = fn => (req, res, next) => fn(req, res, next).catch(next);
 let router = Router();
+/* 변수 체크 */
+const loginVO = new johayoPvs({
+    userId: {type: String, validate: {checkURL: ['!/api/login/token']}},
+    password: {type: String, validate: {checkURL: ['/api/login']}},
+    token: {type:String, validate: {checkURL: ['/api/login/token']}}
+});
+loginVO.setParams = (req, res, next) => {
+    loginVO.set(req, res, next);
+};
 
-router.get('/', (req, res) => res.send('login'));
+/**
+ * 로그인 하기
+ */
+router.post('/', loginVO.setParams, wrap(async function(req, res) {
+    const admin = await LoginService.login(loginVO.get.userId, loginVO.get.password);
+    req.session.admin = admin;
+    res.send(admin);
+}));
+
+/**
+ * 로그인 정보 주기
+ */
+router.get('/', (req, res) => {
+    res.send(req.session['admin']);
+});
+
+/**
+ * 로그인 정보 주기
+ */
+router.post('/token', loginVO.setParams, wrap(async (req, res) => {
+    const admin = await LoginService.tokenLogin(loginVO.get.token);
+    req.session.admin = admin;
+    res.send(admin);
+}));
 
 export = router;
---
+++ server-node/service/loginService.ts
@@ -0,0 +1,45 @@
+/**
+ * Created by mayaj on 2016-04-25.
+ */
+import {Model} from '../module/mongo';
+import {Crypto} from '../module/crypto';
+import {} from '../module/error';
+import {error} from "../module/error";
+
+const Admin = Model.admin;
+const passwordKey = process.env.PASSWORD_KEY || 'testKey';
+
+export module LoginService {
+    /**
+     * 일반적인 로그인
+     * 
+     * @param userId
+     * @param password
+     * @returns {T}
+     */
+    export async function login(userId: string, password: string) {
+        const admin = await Admin.findOne({_id: userId, password: Crypto.encrypt(password, passwordKey)}, {password: -1});
+
+        if(!admin) {
+            error(409, '아이디 / 패스워드를 확인해주세요.');
+        }
+
+        return admin;
+    }
+
+    /**
+     * 토큰 로그인
+     * 
+     * @param token
+     * @returns {T}
+     */
+    export async function tokenLogin(token) {
+        const admin = await Admin.findOne({token: token}, {password: -1});
+        
+        if(!admin) {
+            error(409, '토큰이 잘못되었습니다.');
+        }
+        
+        return admin;
+    }
+}
