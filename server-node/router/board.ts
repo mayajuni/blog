b096b9591b195cd703232ceaf3cbf85a9562fb17
Kwon Dong Jun <mayajuni10@gmail.com>
2016-05-01 00:42:56 +0900

    게시판 추가

--- server-node/router/board.ts
+++ server-node/router/board.ts
@@ -1,3 +1,18 @@
 /**
  * Created by mayaj on 2016-04-24.
  */
+import {Router} from "express";
+import johayoPvs = require("johayo-pvs");
+
+/* 에러시 check를 하여 next(err)을 해준다. */
+import {wrap} from "../module/error";
+import {loginCheck} from "../module/auth";
+
+
+
+/* service */
+import {BoardService} from "../service/boardService";
+
+let router = Router();
+
+export = router;
